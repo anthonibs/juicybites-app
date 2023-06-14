9c1fbeb4af4df491a6bb8bc2ff9d32c46452a064
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-06-14 18:42:22 -0300

    ✨ feat:  Função Switch para retorna o emoji e exibir na tela de notificação

---
+++ src/helpers/snackEmoji.ts
@@ -0,0 +1,14 @@
+export function snackEmoji(name: string) {
+	switch (name.toLocaleLowerCase()) {
+		case 'burger':
+			return '🍔';
+		case 'pizza':
+			return '🍕';
+		case 'drink':
+			return '🍺';
+		case 'ice-cream':
+			return '🍦';
+		default:
+			return '😋';
+	}
+}
