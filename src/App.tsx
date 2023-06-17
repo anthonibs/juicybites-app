5080f3a07094fc9f9b76fd366c51abe8c9910d2a
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2023-06-17 00:52:29 -0300

    ✨ feat:  Consumindo Context Provider no arquivo App para utilizar no components filhos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { BrowserRouter } from 'react-router-dom';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/ReactToastify.min.css';
+
 import { AppRoutes } from './routes';
 
 import { Theme } from './styles/Theme';
 import { GlobalStyle } from './styles/global';
+import { SnackProvider } from 'context/SnackContext';
+import { CartProvider } from 'context/CartContext';
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Theme>
-				<GlobalStyle />
-				<AppRoutes />
+				<SnackProvider>
+					<CartProvider>
+						<ToastContainer autoClose={2000} />
+						<GlobalStyle />
+						<AppRoutes />
+					</CartProvider>
+				</SnackProvider>
 			</Theme>
 		</BrowserRouter>
 	);
